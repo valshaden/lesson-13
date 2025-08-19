22f0af26affb516b21545808a9543200db7616e9
valshaden <valshaden@yandex.ru>
2025-08-19 15:53:57 +0300

    +

---
+++ py2.py
@@ -0,0 +1,23 @@
+from sys import *
+from sys import argv
+import subprocess
+
+#import subprocess
+
+#print(version)
+#print(platform)
+#print(argv[1], argv[2] +"!")
+print(executable)
+#print("Точка выхода")
+#exit('0')
+#subprocess.run("python --version")
+#subprocess.call("python --version")
+code = subprocess.call(["python", "p1.py"])
+#subprocess.call("python", "p1.py")
+print("p1.py вернул", code)
+
+#if platform == "win32":
+#    subprocess.call("mspaint")
+#elif platform.startswith("linux"):
+#    subprocess.call("gnome-calculator")
+#    subprocess.Popen(["ls -l"])
